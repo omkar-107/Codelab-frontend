b8dff60550b666e917638b647ee6fa19ae54e069
Omkar Salunkhe <114748064+omkar-107@users.noreply.github.com>
2023-10-17 10:16:22 +0530

    Update DragAndDrop.jsx

--- src/component/DragAndDrop.jsx
+++ src/component/DragAndDrop.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-const FileDrop = () => {
+const FileDrop = (props) => {
     const [dragging, setDragging] = useState(false);
-    const [fileNames, setFileNames] = useState([]);
-    const acceptedFileTypes = ['text/plain']; // Define accepted file types
+    const [fileNames, setFileNames] = useState("");
+    // const acceptedFileTypes = ['text/plain']; // Define accepted file types
 
     const handleDragEnter = (e) => {
         e.preventDefault();
@@ -20,43 +20,44 @@ const FileDrop = () => {
 
         const files = Array.from(e.dataTransfer.files);
 
-        // Check if dropped files are of the accepted types
-        const validFiles = files.filter(file => acceptedFileTypes.includes(file.type));
-
-        if (validFiles.length > 0) {
-            if (validFiles.length > 1) {
-                alert('Please select only one file');
-            }
-            else {
-                console.log(validFiles[0].type);
-                validFiles.forEach(file => {
-                    const reader = new FileReader();
-
-                    reader.onload = (event) => {
-                        const fileContent = event.target.result;
-                        console.log(fileContent);
-
-                        // You can further process or display the file content as needed
-                    };
-
-                    reader.readAsText(file);
-                });
-                const newFileNames = validFiles.map(file => file.name);
-                setFileNames(newFileNames);
-            }
-        } else {
-            alert('Invalid file type. Please drop a C, C++, Java, or Python source code file.');
+        if (files.length > 1) {
+            alert('Please select only one file');
+            return;
         }
+
+        // Check if dropped files are of the accepted types
+        let extension = files[0].name.substring(files[0].name.lastIndexOf('.') + 1);
+
+        if (!(extension === "cpp" || extension === "c" || extension === "py" || extension === "java")) {
+            alert(`.${extension} file is not allowed! Please select valid file`);
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const fileContent = event.target.result;
+            console.log(fileContent);
+            props.parentCallBack(fileContent, extension);
+
+            // You can further process or display the file content as needed
+        };
+
+        reader.readAsText(files[0]);
+        setFileNames(files[0].name);
+
+
+        // files.length = 0;
     };
 
     return (
-        <div className='flex justify-evenly m-10'>
+        <div className='flex justify-evenly m-10 flex-col md:flex-row lg:flex-row'>
             <div className=' flex flex-col justify-evenly'>
                 <h1 className=' text-4xl font-semibold text-slate-700'>Already Have Code File ... ?</h1>
                 <h1 className=' text-4xl text-slate-800'>Drop it here</h1>
             </div>
             <div
-                className={`${dragging ? 'dragging' : ''} border-2 border-black border-dashed w-[800px] h-[200px] flex flex-col justify-center items-center hover:bg-red-100 transition-all 0.5 text-lg font-medium mt-12`}
+                className={`${dragging ? 'dragging' : ''} border-2 border-black border-dashed w-[100%] md:w-[50%] lg:w-[50%] h-[200px] flex flex-col justify-center items-center hover:bg-red-100 transition-all 0.5 text-lg font-medium mt-12`}
                 onDragEnter={handleDragEnter}
                 onDragOver={handleDragEnter}
                 onDragLeave={handleDragLeave}
@@ -67,11 +68,7 @@ const FileDrop = () => {
                     <div className="file-names">
                         <br></br>
                         <h3>Dropped File:</h3>
-                        <ul>
-                            {fileNames.map((fileName, index) => (
-                                <li key={index}>{fileName}</li>
-                            ))}
-                        </ul>
+                        <p>{fileNames}</p>
                     </div>
                 )}
             </div>
