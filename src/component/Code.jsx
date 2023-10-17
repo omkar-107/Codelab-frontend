f02f8f104b85ae74d7e703994fe69ba5bf37f670
Omkar Salunkhe <114748064+omkar-107@users.noreply.github.com>
2023-10-17 10:15:52 +0530

    Update Code.jsx

--- src/component/Code.jsx
+++ src/component/Code.jsx
@@ -7,6 +7,7 @@ import 'ace-builds/src-noconflict/theme-terminal'
 import 'ace-builds/src-noconflict/theme-kuroir'
 import 'ace-builds/src-noconflict/theme-github_dark'
 import 'ace-builds/src-noconflict/theme-monokai'
+import DragDrop from './DragAndDrop';
 import './CodeStyles.css'
 import Copy from './Copy';
 import axios from 'axios';
@@ -23,10 +24,33 @@ export default class Code extends React.Component {
             theme: 'terminal',
             code: '// your code goes here',
             mode: 'c_cpp',
-            result:'Compile and Execute the code to see output'
+            result: 'Compile and Execute the code to see output',
+            language: 'c'
         }
 
         this.onChange = this.onChange.bind(this);
+    }
+
+    getChildCode = (childcode, extension) => {
+        if (extension === "c" || extension === "cpp") {
+            this.setState({
+                mode: 'c_cpp',
+                language: extension
+            })
+        }
+        else if (extension === "py") {
+            this.setState({
+                mode: 'python',
+                language: 'python'
+            })
+        }
+        else if (extension === 'java') {
+            this.setState({ mode: 'java', language: extension })
+        }
+
+        this.setState({
+            code: childcode
+        })
     }
 
     onChange(newValue) {
@@ -51,9 +75,22 @@ export default class Code extends React.Component {
     }
 
     modeChangeHandler = (event) => {
+        //setting the language 
         this.setState({
-            mode: event.target.value
+            language: event.target.value
         })
+
+        //setting the intellisence mode
+        if (event.target.value === 'c' || event.target.value === 'cpp') {
+            this.setState({
+                mode: 'c_cpp'
+            })
+        }
+        else {
+            this.setState({
+                mode: event.target.value
+            })
+        }
     }
 
     getMode() {
@@ -62,7 +99,7 @@ export default class Code extends React.Component {
 
     async send() {
         try {
-            const tobesent = JSON.stringify({ code: this.state.code, mode: this.state.mode });
+            const tobesent = JSON.stringify({ code: this.state.code, language: this.state.language });
             const options = {
                 headers: { "content-type": "application/json" }
             }
@@ -96,63 +133,69 @@ export default class Code extends React.Component {
 
     render() {
         return (
-            <div className='pt-5'>
-                <div className='flex justify-evenly text-blue-800 text-lg'>
-                    <div className='theme-select outline outline-offset-2 outline-pink-400 rounded-md p-1'>
-                        <label for="theme" className='pr-3'>Theme</label>
+            <div>
+                <div className='pt-5'>
+                    <div className='flex justify-evenly text-blue-800 text-lg'>
+                        <div className='theme-select outline outline-offset-2 outline-pink-400 rounded-md p-1'>
+                            <label for="theme" className='pr-3'>Theme</label>
 
-                        <select name="theme" id="theme" onChange={this.themeChangeHandler}>
-                            <option value="terminal">Terminal</option>
-                            <option value="kuroir">Kuroir</option>
-                            <option value="github_dark">Github Dark</option>
-                            <option value="monokai">Monokai</option>
-                        </select>
+                            <select name="theme" id="theme" onChange={this.themeChangeHandler}>
+                                <option value="terminal">Terminal</option>
+                                <option value="kuroir">Kuroir</option>
+                                <option value="github_dark">Github Dark</option>
+                                <option value="monokai">Monokai</option>
+                            </select>
+                        </div>
+                        <div className='mode-select outline outline-offset-2 outline-pink-400 rounded-md p-1'>
+                            <label for="mode" className='pr-5'>Language</label>
+
+                            <select name="mode" id="mode" value={this.state.language} onChange={this.modeChangeHandler}>
+                                <option value="c">C</option>
+                                <option value="cpp">C++</option>
+                                <option value="python">Python</option>
+                                <option value="java">Java</option>
+                            </select>
+                        </div>
+
                     </div>
-                    <div className='mode-select outline outline-offset-2 outline-pink-400 rounded-md p-1'>
-                        <label for="mode" className='pr-5'>Language</label>
+                    <div className='ace-editor relative'>
+                        <AceEditor
+                            placeholder=""
+                            mode={this.getMode()}
+                            theme={this.gettheme()}
+                            width='100%'
+                            height='600px'
+                            name="blah2"
+                            onLoad={this.onLoad}
+                            onChange={this.onChange}
+                            fontSize={20}
+                            showPrintMargin={true}
+                            showGutter={true}
+                            highlightActiveLine={true}
+                            value={this.getCode()}
+                            setOptions={{
+                                enableBasicAutocompletion: true,
+                                enableLiveAutocompletion: false,
+                                enableSnippets: true,
+                                showLineNumbers: true,
+                                tabSize: 2,
+                            }} />
 
-                        <select name="mode" id="mode" onChange={this.modeChangeHandler}>
-                            <option value="c_cpp">C/C++</option>
-                            <option value="python">Python</option>
-                            <option value="java">Java</option>
-                        </select>
+                        <Copy code={this.state.code} />
+
                     </div>
-
-                </div>
-                <div className='ace-editor relative'>
-                    <AceEditor
-                        placeholder=""
-                        mode={this.getMode()}
-                        theme={this.gettheme()}
-                        width='100%'
-                        height='600px'
-                        name="blah2"
-                        onLoad={this.onLoad}
-                        onChange={this.onChange}
-                        fontSize={20}
-                        showPrintMargin={true}
-                        showGutter={true}
-                        highlightActiveLine={true}
-                        value={this.getCode()}
-                        setOptions={{
-                            enableBasicAutocompletion: true,
-                            enableLiveAutocompletion: false,
-                            enableSnippets: true,
-                            showLineNumbers: true,
-                            tabSize: 2,
-                        }} />
-
-                    <Copy code={this.state.code} />
-
-                </div>
-                <div className='flex justify-center mt-2 mb-2'>
-                    <div className=' flex justify-evenly w-[50%]'>
-                        <button className=' border-2 border-blue-500 rounded-2xl p-2  hover:text-white hover:bg-blue-500 transition-all ease-in-out' onClick={(e) => { console.log("send called"); this.send(); }}>Compile and Execute</button>
+                    <div className='flex justify-center mt-2 mb-2'>
+                        <div className=' flex justify-evenly w-[50%]'>
+                            <button className=' border-2 border-blue-500 rounded-2xl p-2  hover:text-white hover:bg-blue-500 transition-all ease-in-out' onClick={(e) => { console.log("send called"); this.send(); }}>Compile and Execute</button>
+                        </div>
+                    </div>
+                    <div className='h-40 w-full mt-9 border-blue-950' >
+                        <h1 className=' text-lg'>Output: {this.state.result.message}</h1>
+                        <h1 className=' text-lg'>Language: {this.state.result.language}</h1>
                     </div>
                 </div>
-                <div className='h-40 w-full mt-9 border-blue-950' >
-                    <h1 className=' text-lg'>Output: {this.state.result.message}</h1>
-                    <h1 className=' text-lg'>Language: {this.state.result.language}</h1>
+                <div>
+                    <DragDrop parentCallBack={this.getChildCode} />
                 </div>
             </div>
         );
